e1eac8ad2cf593413f3ad570cce01ce6bea376f6
yamada <ymd@m1.local>
2023-04-03 16:46:13 +0900

    Initial Commit

---
+++ RegionQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RegionQuiz
+//
+//  Created by 山田晃路 on 2023/04/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RegionQuiz/RegionQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegionQuizApp.swift
+//  RegionQuiz
+//
+//  Created by 山田晃路 on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct RegionQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
